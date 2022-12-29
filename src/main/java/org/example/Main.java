c249ab6c37ad6ea8cffa47db079d6bd99664ca6e
pochu <jonasz.kasprzak@gmail.com>
2022-12-29 19:39:19 +0100

    pierwszy komit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class Main {
+    private static Cluster cluster;
+private static final String PROPORTIES_FILENAME = "config.proporties";
+    private static Session session;
+    public static void main(String[] args) throws IOException {
+
+    Properties prop=new Properties();
+    String node=prop.getProperty("node1");
+    String port = prop.getProperty("port");
+    prop.load(Main.class.getClassLoader().getResourceAsStream(PROPORTIES_FILENAME));
+
+
+    Cluster.Builder b = Cluster.builder().addContactPoint(node).withCredentials("cassandra", "cassandra");
+    b.withPort(Integer.parseInt(port));
+    cluster = b.build();
+    session = cluster.connect();
+    createKeyspace("test1", "SimpleStrategy",0);
+     close();
+}
+    public Session getSession() {
+        return this.session;
+    }
+
+    public static void close() {
+        session.close();
+        cluster.close();
+    }
+    public static void createKeyspace(
+            String keyspaceName, String replicationStrategy, int replicationFactor) {
+        StringBuilder sb =
+                new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ")
+                        .append(keyspaceName).append(" WITH replication = {")
+                        .append("'class':'").append(replicationStrategy)
+                        .append("','replication_factor':").append(replicationFactor)
+                        .append("};");
+
+        String query = sb.toString();
+        session.execute(query);
+    }
+
+    CREATE TABLE bid_order_by_item_id (
+            item_id int primary key,
+            auction_id int,
+            winning_user_id int,
+            current_price bigint,
+            max_number_of_participants int,
+            participant_count int,
+            bid_start_time date,
+            bid_end_time date,
+            min_bid_amount int,
+            timestamp timestamp
+    );
+}
